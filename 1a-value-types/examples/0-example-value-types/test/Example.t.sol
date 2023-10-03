54bf914d1c41ebaf76f551edad53bdf54d78b195
Dan Nolan <dan.nolan@alchemy.com>
2023-10-03 12:27:49 -0400

    reset tests

--- 1a-value-types/examples/0-example-value-types/test/Example.t.sol
+++ 1a-value-types/examples/0-example-value-types/test/Example.t.sol
@@ -8,7 +8,7 @@ contract ExampleTest is Test {
     Example public example;
 
     function setUp() public {
-        example = new Example(Example.Choice.Down);
+        example = new Example();
     }
 
     function testExample() public {}
